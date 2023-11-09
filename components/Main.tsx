4caf5ff3bda89a3f34707502c7639e5c06cc0aa5
Semenov Volodymyr <trenersem1998@gmail.com>
2023-11-09 00:09:48 -0500

    add about page

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,35 +7,58 @@ const Main = () => {
   const router = useRouter();
   return (
     <>
-    <h1 className="head_text  text-center">
-        Maximize Your {' '}
-        <br className="max-md:hidden"/>
-        <span className="orange_gradient text-center">
-           Short-Term Rental
-        </span>
-        <br />
-        for Less
-      </h1>
-      <p className="desc text-center">
-        Evolve partners with you and takes on the time-consuming tasks of
-        vacation rental management so you can earn more and stress less.
-      </p>
+    <div className='h-[90vh]'>
 
-      <div className='flex-center gap-8 mt-10 mb-20'>
-        <button
-            type='button'
-            className='black_btn'
-            onClick={() => router.push('/form')}
-        >
-            Let's do this
-        </button>
-        <button
-            type='button'
-            className='outline_btn'
-            onClick={() => router.push('/form')}
-        >
-                 Learn more
-        </button>
+      <h1 className="head_text  text-center">
+          Maximize Your {' '}
+          <br className="max-md:hidden"/>
+          <span className="orange_gradient text-center">
+            Short-Term Rental
+          </span>
+          <br />
+          for Less
+        </h1>
+        <p className="desc text-center">
+          Evolve partners with you and takes on the time-consuming tasks of
+          vacation rental management so you can earn more and stress less.
+        </p>
+
+        <div className='flex-center gap-8 mt-10 mb-10'>
+          <button
+              type='button'
+              className='black_btn'
+              onClick={() => router.push('/form')}
+          >
+              Let's do this
+          </button>
+          <button
+              type='button'
+              className='outline_btn'
+              onClick={() => router.push('/form')}
+          >
+                  Learn more
+          </button>
+        </div>
+    </div>
+      
+      <div className='flex flex-col md:flex-row gap-10 layout min-h-[80vh]'>
+            <div className='max-w-[570px] max-h-[390px] w-full h-full mt-10'>
+                <img
+                    src={`https://www-vacasa.imgix.net/people_fatherson_S.jpeg?ar=1600%3A1200&auto=format%2Ccompress&crop=focalpoint&fit=crop&fp-x=0.24&fp-y=0.41&fp-z=1&ixlib=python-3.2.1&q=45&w=1080`}
+                    alt='about us img'
+                    className='min-w-full min-h-full rounded-lg'
+                />
+            </div>
+            <div className='flex-1 w-full md:max-w-[40%]  mx-auto'>
+              <h2 className='font-display text-2xl font-extrabold leading-tight text-black sm:text-3xl sm:leading-tight mb-5'>
+                Over 92% of PremierFloridaBNB markets out-booked other property managers
+              </h2>
+              <h4 className='font-semibold text-gray-500 md:text-lg pb-5  md:text-start mb-5'>
+                PremierFloridaBNB wins more for homeowners
+              </h4>
+              PremierFloridaBNB is a leading property management company specializing in short-term vacation rentals. We are committed to delivering exceptional service, maximizing occupancy rates, and ensuring a seamless experience for both property owners and guests.
+              Our team holds the prestigious SuperHost status, which translates to increased bookings and improved search engine visibility. We utilize automated messaging systems, respond promptly to inquiries, and offer a wide range of services, from check-out calls to quick problem resolution.
+            </div>
       </div>
     </>
   )
--- components/Testitominals.tsx
+++ components/Testitominals.tsx
@@ -1,6 +1,47 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+
+const airbnbID = [
+    {
+        id: '979826132485969238',
+    }
+]
+const apiKey = 'cdf19c2519msha09ba2add6a2d6bp15fae6jsnd00615ffbe88';
+const propertyId = '979826132485969238';
+
+const url = `https://airbnb19.p.rapidapi.com/api/v1/getPropertyReviews?propertyId=${propertyId}`;
+
+const headers = new Headers({
+  'X-RapidAPI-Key': apiKey,
+  'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+});
+
+const options = {
+  method: 'GET',
+  headers: headers,
+};
 
 const Testitominals = () => {
+    const [propertyReviews, setPropertyReviews] = useState();
+    // React.useEffect(() => {
+
+    //     const getData = async () => {
+    //         try {
+    //             const response = await fetch(url, options);
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! Status: ${response.status}`);
+    //         }
+
+    //         const data = await response.json();
+    //         setPropertyReviews(data);
+    //         console.log(data);
+
+    //         } catch (error) {
+    //           console.error(error);
+    //         }
+    //     }
+    //     getData()
+    // }, [])
   return (
     <section className='mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 pt-20'>
         <div className='mx-auto max-w-md text-center sm:max-w-xl'>
@@ -17,8 +58,29 @@ const Testitominals = () => {
             </p>
         </div>
 
-        <div>
+        <div className='hidden space-y-6 py-8 sm:block sm:columns-2 sm:gap-6 lg:columns-3'>
+            {propertyReviews && propertyReviews.map((el: any) => (
+                <div className='relative lg:top-12'>
+                    <div className='not-prose break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter relative lg:top-12'>
+                        <div>
+                            <div className='flex items-center justify-between'>
+                                <div className='flex items-center space-x-2'>
+                                    <img className='h-10 w-10 overflow-hidden border border-gray-200 transition-all ease-in-out hover:scale-105 rounded-md blur-0'/>
+                                </div>
+                            </div>
+                            <div className="mb-2 mt-4 truncate whitespace-pre-wrap text-[15px] text-gray-700">
+                                <a className='font-normal text-[rgb(29,161,242)] no-underline'>
 
+                                </a>
+                                <p>
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            ))}
         </div>
       
     </section>
